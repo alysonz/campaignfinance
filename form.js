298ed530086f112d726815885eaf616eb7326638
alysonz <alysonz@gmail.com>
2014-03-07 18:51:54 +0000

    add data tables

--- form.js
+++ form.js
@@ -156,8 +156,8 @@ $(document).ready(function() {
             var dataArray = jQuery.parseJSON(result);
             //get committee id from results
             var committeeID = dataArray[0][0];
-            //set locator for paragraph with class if committee id
-            var paragraph = $('#results').find("."+committeeID);
+            //set locator for paragraph with class of committee id
+            var paragraph = $('#results').find(".id"+committeeID);
             //prepare existing committee tabs to receive siblings by removing all selected styling
             $('#results').find('#data').children().removeClass('show');
             $('#results').find("#tabBar").children().removeClass('highlight');
@@ -166,16 +166,16 @@ $(document).ready(function() {
               //give results to if statement (for some reason it was undefined if I didn't do this again)
               var dataArray = jQuery.parseJSON(result);
               var committeeID = dataArray[0][0];
-              var paragraph = $('#data').find("."+committeeID);
+              var paragraph = $('#data').find(".id"+committeeID);
               console.log(dataArray);
               //in tab bar, create a new overall tab container with class and data with committee id
-              $('#tabBar').append('<div id="tab" data-id="'+committeeID+'" class="'+committeeID+'"></div>');
+              $('#tabBar').append('<div id="tab" data-id="'+committeeID+'" class="id'+committeeID+'"></div>');
               //add close tab image to tab container
-              $('#tabBar').find("."+committeeID).append('<img src="close.png">'); 
+              $('#tabBar').find(".id"+committeeID).append('<img src="close.png">'); 
               //if the second list contains only one item, it's an error message
               if (dataArray[2].length < 2) {
                 //so in the main data field, add a containder for the results with the class of the committee id
-                $('#data').append('<div id="dataResult" class="'+committeeID+'">'+'</div>');
+                $('#data').append('<div id="dataResult" class="id'+committeeID+'">'+'</div>');
                 console.log(formData);
                 var id = dataArray[0][0];
                 //grab data array from the committee result input checkbox with the same committee id
@@ -183,25 +183,40 @@ $(document).ready(function() {
                 //select committee name value from that data array for the results headline since we can't find it in the error results
                 var committeeName = committeeArray["committeename"]
                 //add error text to tab
-                $('#tabBar').find("."+committeeID).append('<div id="tabName" class="error">Error</div>');
+                $('#tabBar').find(".id"+committeeID).append('<div id="tabName" class="error">Error</div>');
                 //set headline so we know which committee selection threw the error
-                $('#data').find("."+committeeID).append('<h3>'+committeeName+'</h3>');
+                $('#data').find(".id"+committeeID).append('<h3>'+committeeName+'</h3>');
                 //print out results we did get back since that should contain a detailed error message from cfquery.py
-                $('#data').find("."+committeeID).append('<p class="dataResult error">'+dataArray[2]+'</p>');
+                $('#data').find(".id"+committeeID).append('<p class="dataResult error">'+dataArray[2]+'</p>');
                 
               }
               //if the data does look like it has returned without an 'undefined' error
               else {
                 //set committee name from results
-                var committeeName = dataArray[2][1];
+		var committeeName = dataArray[2][1];
+                dataArray.splice(0,1);
+                console.log(dataArray);
                 //set tab to contain committee name 
-                $('#tabBar').find("."+committeeID).append('<div id="tabName">'+committeeName+'</div>');
+                $('#tabBar').find(".id"+committeeID).append('<div id="tabName">'+committeeName+'</div>');
                 //create results container with id of committee
-                $('#data').append('<div id="dataResult" class="'+committeeID+'">'+'</div>');
+                $('#data').append('<div id="dataResult" class="id'+committeeID+'">'+'</div>');
                 //insert committee name as headline, link for downloading from download.CGI with the same formData, and add results array
-                $('#data').find("."+committeeID).append('<h3>'+committeeName+'</h3>');
-                $('#data').find("."+committeeID).append('<a href="http://wildfire.codercollective.org/testcampaignfinance/download.cgi?'+formData+'">Download Data</a>')
-                $('#data').find("."+committeeID).append('<p class="dataResult">'+dataArray+'</p>');
+                $('#data').find(".id"+committeeID).append('<h3>'+committeeName+'</h3>');
+                $('#data').find(".id"+committeeID).append('<a href="http://wildfire.codercollective.org/testcampaignfinance/download.cgi?'+formData+'">Download Data</a>')
+                d3.select('#data .id'+committeeID)
+                .append('table')
+                .style('border-collapse', 'collapse')
+                .style('border', '2px black solid')
+                .selectAll('tr')
+                .data(dataArray)
+                .enter().append('tr')
+                .selectAll('td')
+                .data(function(d){return d;})
+                .enter().append("td")
+                .style('border', '1px black solid')
+                .style('padding', '10px')
+                .text(function(d) {return d;})
+                .style('font-size', "14px");
               }
             }
             //if this is a duplicated request, ie, there is already a tab with results for the committee in question
@@ -211,28 +226,43 @@ $(document).ready(function() {
               //if the duplication is now an error
               if (dataArray[2].length < 2) {
                 //if the old tab did not have error text, replace text with error
-                if(($('#tabBar').find("."+committeeID).find(".error")).length < 1) {
-                  $('#tabBar').find("."+committeeID).find("#tabName").remove();
-                  $('#tabBar').find("."+committeeID).append('<div id="tabName" class="error">Error</div>');
+                if(($('#tabBar').find(".id"+committeeID).find(".error")).length < 1) {
+                  $('#tabBar').find(".id"+committeeID).find("#tabName").remove();
+                  $('#tabBar').find(".id"+committeeID).append('<div id="tabName" class="error">Error</div>');
                 }
                 //leave headline in place but remove download link and data results
-                $('#data').children('.'+committeeID).children('p').remove();
-                $('#data').children('.'+committeeID).children('a').remove();
+                $('#data').children('.id'+committeeID).children('table').remove();
+                $('#data').children('.id'+committeeID).children('a').remove();
                 //replace data results with error message from cfquery.py
-                $('#data').find("."+committeeID).append('<p class="dataResult error">'+dataArray[2]+'</p>');
+                $('#data').find(".id"+committeeID).append('<p class="dataResult error">'+dataArray[2]+'</p>');
               }
               //if results are not an error
               else {
               var committeeName = dataArray[2][1];
               //but the previous results /were/ an error
-              if(($('#tabBar').find("."+committeeID).find(".error")).length > 0) {
+              if(($('#tabBar').find(".id"+committeeID).find(".error")).length > 0) {
                 //replace error text with the committee name
-                $('#tabBar').find("."+committeeID).find("#tabName").remove();
-                $('#tabBar').find("."+committeeID).append('<div id="tabName">'+committeeName+'</div>');
+                $('#tabBar').find(".id"+committeeID).find("#tabName").remove();
+                $('#tabBar').find(".id"+committeeID).append('<div id="tabName">'+committeeName+'</div>');
+                $('#data').children('.id'+committeeID).children('p').remove();
               }
-              //load replace old results with new results
-              $('#data').children('.'+committeeID).children('p').remove();
-              $('#data').children('.'+committeeID).append('<p class="dataResult">'+dataArray+'</p>');  
+              //replace old results with new results
+              dataArray.splice(0,1);
+              $('#data').children('.id'+committeeID).children('table').remove();
+              d3.select('#data .id'+committeeID)
+              .append('table')
+              .style('border-collapse', 'collapse')
+              .style('border', '2px black solid')
+              .selectAll('tr')
+              .data(dataArray)
+              .enter().append('tr')
+              .selectAll('td')
+              .data(function(d){return d;})
+              .enter().append("td")
+              .style('border', '1px black solid')
+              .style('padding', '10px')
+              .text(function(d) {return d;})
+              .style('font-size', "14px");
               }
             }
           },
@@ -282,7 +312,7 @@ $(document).ready(function() {
           //grab individual name from results
           var name = dataArray[0][0]+ dataArray[0][1];
           //set variable to locate class with individual name to see if a tab is already open
-          var paragraph = $('#results').find("."+name);
+          var paragraph = $('#results').find(".id"+name);
           //remove all select styles
           $('#results').find('#data').children().removeClass('show');
           $('#results').find("#tabBar").children().removeClass('highlight');
@@ -295,18 +325,32 @@ $(document).ready(function() {
             //make a class name without a space
             var name = dataArray[0][0]+ dataArray[0][1];
             //add a tab container with class of individual
-            $('#tabBar').append('<div id="tab" data-id="'+name+'" class="'+name+'">'+'</div>');
+            $('#tabBar').append('<div id="tab" data-id="'+name+'" class="id'+name+'">'+'</div>');
             //add close tab image
-            $('#tabBar').find("."+name).append('<img src="close.png">');
+            $('#tabBar').find(".id"+name).append('<img src="close.png">');
             //add tab text container and tab name
-            $('#tabBar').find("."+name).append('<div id="tabName">'+tabName+'</div>');
+            $('#tabBar').find(".id"+name).append('<div id="tabName">'+tabName+'</div>');
             //add data result container with class of individual
-            $('#data').append('<div id="dataResult" class="'+name+'">'+'</div>');
-            var paragraph = $('#data').find("."+name);
+            $('#data').append('<div id="dataResult" class="id'+name+'">'+'</div>');
+            var paragraph = $('#data').find(".id"+name);
             //add headline, download link and data results and hide all tabs
             $(paragraph).append("<h3>"+tabName+"</h3>");
             $(paragraph).append('<a href="http://wildfire.codercollective.org/testcampaignfinance/download.cgi?'+formData+'">Download Data</a>');
-            $(paragraph).append('<p class="dataResult">'+dataArray+"</p>");
+            dataArray.splice(0,1);
+            d3.select('#data .id'+name)
+            .append('table')
+            .style('border-collapse', 'collapse')
+            .style('border', '2px black solid')
+            .selectAll('tr')
+            .data(dataArray)
+            .enter().append('tr')
+            .selectAll('td')
+            .data(function(d){return d;})
+            .enter().append("td")
+            .style('border', '1px black solid')
+            .style('padding', '10px')
+            .text(function(d) {return d;})
+            .style('font-size', "14px");
             $('#data').children().addClass('hide');
           }
           //if a tab does exist for the searched individual
@@ -314,8 +358,22 @@ $(document).ready(function() {
             var dataArray = jQuery.parseJSON(result);
             var name = dataArray[0][0]+ dataArray[0][1];
             //remove existing data from results container and replace with new results
-            $('#data').children('.'+name).children('p').remove();
-            $('#data').children('.'+name).append('<p class="dataResult">'+dataArray+'</p>');
+            dataArray.splice(0,1);
+            $('#data').children('.id'+name).children('table').remove();
+            d3.select('#data .id'+name)
+            .append('table')
+            .style('border-collapse', 'collapse')
+            .style('border', '2px black solid')
+            .selectAll('tr')
+            .data(dataArray)
+            .enter().append('tr')
+            .selectAll('td')
+            .data(function(d){return d;})
+            .enter().append("td")
+            .style('border', '1px black solid')
+            .style('padding', '10px')
+            .text(function(d) {return d;})
+            .style('font-size', "14px");
           }
         },
         //remove working animation, hide all tabs, show first tab and first data result
@@ -343,8 +401,8 @@ $(document).ready(function() {
     $('#results').find('#data').children().removeClass('show');
     $('#results').find('#data').children().addClass('hide');
     //remove hide from the results with class matching tab data and add show
-    $('#data').find("."+committeeData["id"]).removeClass('hide');
-    $('#data').find("."+committeeData["id"]).addClass('show'); 
+    $('#data').find(".id"+committeeData["id"]).removeClass('hide');
+    $('#data').find(".id"+committeeData["id"]).addClass('show'); 
   });
   //on clicking the close image in the tab
   $('#tabBar').on('click', 'img',  function () {
@@ -354,13 +412,13 @@ $(document).ready(function() {
     //if parent is not highlighted, remove tab and corresponding results container
     if (committeeTab.length < 1) {
       $(this).closest('#tab').remove();
-      $('#data').find("."+committeeData["id"]).remove();
+      $('#data').find(".id"+committeeData["id"]).remove();
     }
     //if the tab container is highlighted
     else {
       //remove tab and corresponding data result, highligh first tab and show first data result
       $(this).closest('#tab').remove();
-      $('#data').find("."+committeeData["id"]).remove();
+      $('#data').find(".id"+committeeData["id"]).remove();
       $('#data').find('#dataResult').removeClass('hide');
       $('#data').find('#dataResult').addClass('show');
       $('#tabBar').find("#tab").addClass('highlight');
