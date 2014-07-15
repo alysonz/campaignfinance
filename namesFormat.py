fe152f9ef2639e0bbe03b1d1c42ae693bd52a36d
alysonz <alysonz@gmail.com>
2014-07-15 22:57:43 +0000

    actually remove unrelated files

--- namesFormat.py
+++
@@ -1,7 +0,0 @@
-names = open("namesConsolidated.txt","r")
-namesFormat = open("namesFormat.txt","w")
-for line in names:
-	items = line.split("|")
-	line = "|".join(items)
-	line = line.rstrip()
-	namesFormat.write("%s\n" % (line))
